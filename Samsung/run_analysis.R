d7c683fddaae2531993f44a62e8baecffded9e78
Murthy Gudipati <mgudipati@saventech.com>
2014-04-22 10:38:21 +0530

    loaded test files into a test data frame.

--- Samsung/run_analysis.R
+++ Samsung/run_analysis.R
@@ -3,15 +3,19 @@ run_analysis <- function() {
   
   # Load the features.txt file for column names.
   features = read.table("./features.txt")
-  
-  # Load the x test file using the column names from features list.
-  xtest = read.table("./X_test.txt", col.names=features[[2]])
 
-  # Load the x train file.
-  xtrain = read.table("./X_train.txt", col.names=features[[2]])
-  
   # Load the activity labels from the file.
   actlabs = read.table("./activity_labels.txt")
   
-  # Load the y test file 
+  # Create test data frame by loading the subject test file.
+  har.test = read.table("./subject_test.txt", col.names="Subject")  
+  
+  # Load the y test file.
+  ytest = read.table("./y_test.txt")
+  
+  # Add Activity column to the test data frame using activity labels.
+  har.test$Activity = factor(ytest[[1]], labels=actlabs[[2]])
+  
+  # Load the x test file into test data frame using the column names from features list.
+  har.test[as.character(features[[2]])] = read.table("./X_test.txt")  
 }
